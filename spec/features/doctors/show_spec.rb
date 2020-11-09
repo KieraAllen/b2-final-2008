9dbf12b05a157fc6d8080c711c4c55d16ab62b19
Kiera Allen <kieraaallen@gmail.com>
2020-11-09 11:40:19 -0700

    Fix doctor patient relationship and create doctor_patients join table

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -7,9 +7,13 @@ RSpec.describe "As a visitor" do
 
       @meredith = Doctor.create!(name: "Meridith Grey", specialty: "General Surgery", university: "Dartmouth", hospital_id: @greys.id)
 
-      @patient_1 = Patient.create!(name: "Beatrix Potter", age: 45, doctor_id: @meredith.id)
-      @patient_2 = Patient.create!(name: "Ramona Quimby", age: 12, doctor_id: @meredith.id)
-      @patient_3 = Patient.create!(name: "Herman Munster", age: 100, doctor_id: @meredith.id)
+      @patient_1 = Patient.create!(name: "Beatrix Potter", age: 45)
+      @patient_2 = Patient.create!(name: "Ramona Quimby", age: 12)
+      @patient_3 = Patient.create!(name: "Herman Munster", age: 100)
+
+      DoctorPatient.create!(doctor_id: @meredith.id, patient_id: @patient_1.id)
+      DoctorPatient.create!(doctor_id: @meredith.id, patient_id: @patient_2.id)
+      DoctorPatient.create!(doctor_id: @meredith.id, patient_id: @patient_3.id)
     end
 
     it "can see doctor's name, specialty, and university, the hospital where they work, and the names of all their patients" do
@@ -27,7 +31,7 @@ RSpec.describe "As a visitor" do
 
     it "has a button that removes patient from doctor's caseload next to each patient" do
       visit "/doctors/#{@meredith.id}"
-save_and_open_page
+
         within("#patient-#{@patient_1.id}") do
           expect(page).to have_button("Remove")
         end
@@ -39,6 +43,20 @@ save_and_open_page
         within("#patient-#{@patient_3.id}") do
           expect(page).to have_button("Remove")
         end
+    end
+
+    it "can click 'Remove' button, return to doctor's show page, and the patient is no longer listed" do
+
+      visit "/doctors/#{@meredith.id}"
+
+        within("#patient-#{@patient_1.id}") do
+          click_button("Remove")
+        end
+
+      expect(current_path).to eq("/doctors/#{@meredith.id}")
+      expect(page).to have_content(@patient_2.name)
+      expect(page).to have_content(@patient_3.name)
+      expect(page).to_not have_content(@patient_1.name)
     end
   end
 end
