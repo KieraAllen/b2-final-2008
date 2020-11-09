ffb6fec24c2fb497edc733aa88fe1e26ff7427ac
Kiera Allen <kieraaallen@gmail.com>
2020-11-09 09:23:58 -0700

    Create models and add relationships

---
+++ app/models/doctor.rb
@@ -0,0 +1,5 @@
+class Doctor < ApplicationRecord
+  belongs_to :hospital
+  has_many :patients
+
+end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Dcotor, type: :model do
+describe Doctor, type: :model do
   describe "relationships" do
     it { should belong_to :hospital }
     it { should have_many :patients }
