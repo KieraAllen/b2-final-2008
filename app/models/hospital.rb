f5a4941b9395bfbb52e3ecce4c4a15a967444119
Kiera Allen <kieraaallen@gmail.com>
2020-11-09 10:40:34 -0700

    Create and test model methods

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,11 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def total_doctors
+    doctors.count
+  end
+
+  def unique_universities
+    doctors.distinct.pluck(:university)
+  end
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,25 @@ describe Hospital, type: :model do
   describe "relationships" do
     it { should have_many :doctors }
   end
+
+  describe "instance methods" do
+    before :each do
+      @greys = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      @meredith = Doctor.create!(name: "Meridith Grey", specialty: "General Surgery", university: "Dartmouth", hospital_id: @greys.id)
+
+      @teddy = Doctor.create!(name: "Teddy Altman", specialty: "Cardiothoracic Surgery", university: "George Washington University", hospital_id: @greys.id)
+
+      @knox = Doctor.create!(name: "Knox", specialty: "Anesthesiology", university: "Dartmouth", hospital_id: @greys.id)
+    end
+
+    it "#total_doctors" do
+      expect(@greys.total_doctors).to eq(3)
+    end
+
+    it "#unique_universities" do
+      expected = ['Dartmouth', 'George Washington University']
+
+      expect(@greys.unique_universities).to eq(expected)
+    end
+  end
 end
